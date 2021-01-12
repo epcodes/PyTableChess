0957f907e7bc587e07bb49ba22bfffbcdd1e9c2c
epark <69566231+epcodes@users.noreply.github.com>
2021-01-12 14:47:32 -0800

    Added opening detection for all 33 current openings

--- book.py
+++ book.py
@@ -16,4 +16,79 @@ book = ["rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w KQkq c6 0 2", "rnbqkb
 "rnbqkbnr/pppppppp/8/8/8/1P6/P1PPPPPP/RNBQKBNR b KQkq - 0 1", "rnbqkbnr/pppppppp/8/8/1P6/8/P1PPPPPP/RNBQKBNR b KQkq b3 0 1",
 "rnbqkbnr/pppppppp/8/8/6P1/8/PPPPPP1P/RNBQKBNR b KQkq g3 0 1" 
 ]
+
+def get_opening():
+    fen = input("Input FEN from chess.com")
+    if fen == book[0]:
+        print("Sicilian Defense")
+    elif fen == book[1]:
+        print("French Defense")    
+    elif fen == book[2]:
+        print("Ruy López Opening")   
+    elif fen == book[3]:
+        print("Caro-Kann Defense")   
+    elif fen == book[4]:
+        print("Italian Game")   
+    elif fen == book[5]:
+        print("Sicilian Defense: Closed")   
+    elif fen == book[6]:
+        print("Scandinavian Defense")   
+    elif fen == book[7]:
+        print("Pirc Defense: 2.d4 Nf6")   
+    elif fen == book[8]:
+        print("Sicilian Defense: Alapin Variation")   
+    elif fen == book[9]:
+        print("Alekhine's Defense")                                   
+    elif fen == book[10]:
+        print("King's Gambit")   
+    elif fen == book[11]:
+        print("Scotch Game")   
+    elif fen == book[12]:
+        print("Queen's Gambit")   
+    elif fen == book[13]:
+        print("Slav Defense")   
+    elif fen == book[14]:
+        print("King's Indian Defense")   
+    elif fen == book[15]:
+        print("Nimzo-Indian Defense")   
+    elif fen == book[16]:
+        print("Queen's Indian Defense")   
+    elif fen == book[17]:
+        print("Bogo-Indian Defense") 
+    elif fen == book[18]:
+        print("Grünfeld Defense")   
+    elif fen == book[19]:
+        print("Dutch Defense")   
+    elif fen == book[20]:
+        print("Trompowsky Attack")   
+    elif fen == book[21]:
+        print("Benko Gambit")   
+    elif fen == book[22]:
+        print("London System")                                   
+    elif fen == book[23]:
+        print("Benoni Defense: Modern Variation, 4.Nc3 exd5 5.cxd5 d6")   
+    elif fen == book[24]:
+        print("Catalan Opening")   
+    elif fen == book[25]:
+        print("Réti Opening")   
+    elif fen == book[26]:
+        print("English Opening")   
+    elif fen == book[27]:
+        print("Bird's Opening")   
+    elif fen == book[28]:
+        print("King's Indian Attack")   
+    elif fen == book[29]:
+        print("King's Fianchetto Opening")   
+    elif fen == book[30]:
+        print("Nimzowitsch-Larsen Attack")    
+    elif fen == book[31]:
+        print("Polish Opening") 
+    elif fen == book[32]:
+        print("Grob Opening")                                           
+                                   
+
+        
+
+#get_opening()        
 #print(book)
+
